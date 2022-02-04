3b80e96b357623b84090fcb3c80c2f9bfa90246f
ChocolateSocrotes <52463084+DunkanGibson@users.noreply.github.com>
2022-02-04 17:02:19 -0500

    Update disc_2.js
    Updated the function to parse string into Integers so addition works

--- Chapter 2/Discussion 1/disc_2.js
+++ Chapter 2/Discussion 1/disc_2.js
@@ -1,7 +1,7 @@
 function calculate() {
 var num1 = document.getElementById("num1").value;
 var num2 = document.getElementById("num2").value;
-var total = num1 + num2;
+var total = parseInt(num1) + parseInt(num2);
 var output = "The total is " + total;
 document.getElementById("output").innerHTML = output;
 }
